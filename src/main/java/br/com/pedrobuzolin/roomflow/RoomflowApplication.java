121168a2bebc4ea51d9cbc9c790f7d5926752354
pedrobuzolin <buzolinpedro@gmail.com>
2026-07-09 15:45:16 -0300

    Inicio Projeto

---
+++ src/main/java/br/com/pedrobuzolin/roomflow/RoomflowApplication.java
@@ -0,0 +1,13 @@
+package br.com.pedrobuzolin.roomflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomflowApplication.class, args);
+	}
+
+}
