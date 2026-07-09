5aae90ff2f1673bc8b3e906cc3a5ffe2da0bbf98
pedrobuzolin <buzolinpedro@gmail.com>
2026-07-09 17:57:27 -0300

    Adiciona Excecao de Validacao

---
+++ src/main/java/br/com/pedrobuzolin/roomflow/api/exception/ValidacaoException.java
@@ -0,0 +1,7 @@
+package br.com.pedrobuzolin.roomflow.api.exception;
+
+public class ValidacaoException extends RuntimeException {
+    public ValidacaoException(String message) {
+        super(message);
+    }
+}
